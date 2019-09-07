5e1415d221d7289cb6d27505ccb0206b6e233074
arley <34805850@qq.com>
2019-09-07 22:13:16 +0800

    race over, add p ~

---
+++ race.js
@@ -0,0 +1,23 @@
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("p1 resolve");
+    resolve("p1");
+  }, 100);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("p2 resolve");
+    resolve("p2");
+  }, 200);
+});
+
+const p = Promise.race([p1, p2])
+  .then(data => {
+    console.log("JSLog: data", data);
+  })
+  .catch(err => {
+    console.log("JSLog: err", err);
+  });
+
+  console.log('p~')
